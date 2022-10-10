1341d526f1903585deb3106547db0d56b54cb250
stan2712 <staann27@gmail.com>
2022-10-10 14:18:30 +0100

    removed unused error handling and console log

--- api/app.js
+++ api/app.js
@@ -8,37 +8,11 @@ app.use(express.json());
 app.get("/api/categories", getCategories);
 
 //404 wrong path
-app.all("*", (req, res, next) => {
+app.all("/api/*", (req, res, next) => {
   res.status(404).send({
     msg: "Page not found",
   });
 });
 
-//javascript
-
-app.use((err, req, res, next) => {
-  if (err.status) {
-    res.status(err.status).send({ msg: err.msg });
-  } else {
-    next(err);
-  }
-});
-
-// psql error
-
-app.use((err, req, res, next) => {
-  if (err.code === "22P02") {
-    res.status(400).send({ msg: "bad request" });
-  } else {
-    next(err);
-  }
-});
-
-//500 error
-
-app.use((err, req, res, next) => {
-  console.log(err);
-  res.status(500).send({ msg: "internal server error" });
-});
 
 module.exports = app;
--- api/models/categoriesmodel.js
+++ api/models/categoriesmodel.js
@@ -2,7 +2,6 @@ const db = require("../../db/connection");
 
 exports.selectCategories = () => {
   return db.query("select * from categories").then(({ rows }) => {
-    // console.log(rows, "mod");
     return rows;
   });
 };
