9600056497107644e85c5f00f28f5e2c61ccb98f
stan2712 <staann27@gmail.com>
2022-10-10 13:47:21 +0100

    readded catch block

--- api/controllers/categoriescontroller.js
+++ api/controllers/categoriescontroller.js
@@ -1,10 +1,11 @@
 const { selectCategories } = require("../models/categoriesmodel");
 
 exports.getCategories = (req, res) => {
-  selectCategories().then((categories) => {
-    res.status(200).send(categories);
-  });
-  //   .catch((err) => {
-  //     next(err);
-  //   });
+  selectCategories()
+    .then((categories) => {
+      res.status(200).send(categories);
+    })
+    .catch((err) => {
+      next(err);
+    });
 };
